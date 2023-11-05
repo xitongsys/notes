5e57912217f3d1ac48e630971cb7e681c3889008
xitongsys <xitongsys@gmail.com>
2023-11-05 14:09:19 +0800

    ..

--- computer/algorithm/codetemplates.cc
+++ computer/algorithm/codetemplates.cc
@@ -158,4 +158,92 @@ public:
 
 /////////////////////
 
+// GCD /////////////
+
+ll mpow(ll a, ll n, ll mod)
+{
+    if (n == 0)
+        return 1;
+    ll r = mpow(a, n / 2, mod);
+    r *= r;
+    r %= mod;
+    if (n % 2)
+        r *= a;
+    return r % mod;
+}
+
+ll inv(ll a, ll mod)
+{
+    return mpow(a, mod - 2, mod);
+}
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll extend_gcd(ll a, ll b, ll& x, ll& y)
+{
+    if (b == 0) {
+        x = 1, y = 0;
+        return a;
+    } else {
+        ll r = extend_gcd(b, a % b, y, x);
+        y -= x * (a / b);
+        return r;
+    }
+}
+ll inv(ll a, ll mod)
+{
+    ll x, y;
+    extend_gcd(a, mod, x, y);
+    x = (x % mod + mod) % mod;
+    return x;
+}
+
+//////////////////////////
+
+// DiffArray /////////////
+
+template <class T>
+class DiffArray {
+public:
+    int n;
+    vector<T> ds;
+    DiffArray(int n, int v = 0)
+    {
+        this->n = n;
+        ds = vector<T>(n, v);
+    }
+
+    void add(int b, int e, T v)
+    {
+        if (b > e) {
+            return;
+        }
+        ds[b] += v;
+        if (e + 1 < n) {
+            ds[e + 1] -= v;
+        }
+    }
+
+    vector<T> sum()
+    {
+        vector<T> res = ds;
+        for (int i = 1; i < n; i++) {
+            res[i] += res[i - 1];
+        }
+        return res;
+    }
 };
+
+////////////////////////////
+
+};
+
+int main()
+{
+    return 0;
+}
